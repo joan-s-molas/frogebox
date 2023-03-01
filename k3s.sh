f9428dbc4ba7cd9273eabaa3480d28e673635b48
Debian <debian@packer-output-0837840a-29ed-4692-a149-04011bdf0af7>
2023-03-01 11:37:41 +0000

    peepeepoopoo

--- k3s.sh
+++ k3s.sh
@@ -3,5 +3,6 @@
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-backend none \
     --disable-network-policy \
     --cluster-cidr=10.0.0.0/8 \
-    --disable=traefik
+    --disable=traefik \
+    --disable=servicelb
     " sh -s -
