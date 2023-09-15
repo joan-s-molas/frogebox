32f8676bd5719262b6e28347cd202cf33a80ac7b
Debian <debian@packer-output-0837840a-29ed-4692-a149-04011bdf0af7>
2023-09-15 13:26:20 +0000

    --wip-- [skip ci]

--- joan.io/src/App.tsx
+++ joan.io/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [fontSize, setFontSize] = useState<number>(Math.min(window.innerHeight, window.innerWidth) / 4);
+
+  const adjustFontSize = () => {
+    setFontSize(Math.min(window.innerHeight, window.innerWidth) / 4);
+  };
+
+  useEffect(() => {
+    adjustFontSize();  // Adjust font size after the component is mounted
+
+    window.addEventListener('resize', adjustFontSize);
+
+    return () => {
+      window.removeEventListener('resize', adjustFontSize);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <div className="full-screen-joan">JOAN</div>
+      <div className="full-screen-joan" style={{ fontSize: `${fontSize}px` }}>JOAN</div>
     </div>
   );
 }
