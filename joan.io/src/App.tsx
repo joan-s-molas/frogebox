86f577850ecd4a84f76e0686b55dca0c111638ab
Debian <debian@packer-output-0837840a-29ed-4692-a149-04011bdf0af7>
2023-09-15 15:19:06 +0000

    --wip-- [skip ci]

--- joan.io/src/App.tsx
+++ joan.io/src/App.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 const App: React.FC = () => {
   const [fontSize, setFontSize] = useState<number>(Math.min(window.innerHeight, window.innerWidth) / 4);
@@ -21,6 +24,14 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="full-screen-joan" style={{ fontSize: `${fontSize}px` }}>JOAN</div>
+      <div className="underneath">
+        <a href="https://github.com/joan-s-molas" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGithub} style={{color: "#e03616", marginRight: "10px"}} size="xl" />
+        </a>
+        <a href="https://www.linkedin.com/in/joan-serra-85b1bb41/" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} style={{color: "#e03616"}} size="xl" />
+        </a>
+      </div>
     </div>
   );
 }
